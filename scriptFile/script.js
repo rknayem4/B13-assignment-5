14027a7b9e982645b296a15421db82446321e841
rknayem4 <rknayem10@gmail.com>
2026-03-08 15:36:04 +0600

    added status dynamicly by map

--- scriptFile/script.js
+++ scriptFile/script.js
@@ -1,32 +1,41 @@
+let allIssues = [];
+
+const problemStatus = (arr) => {
+  const element = arr.map(
+    (
+      el,
+    ) => ` <p class="py-1 px-3 font-semibold bg-[#ef444430] text-[#ef4444] rounded-2xl ">${el.toUpperCase()}</p> `,
+  );
+  return element.join(" ");
+};
 const loadData = () => {
   const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
   fetch(url)
     .then((res) => res.json())
     .then((output) => {
-      displayShow(output.data);
+      allIssues = output.data;
+      displayShow(allIssues);
+      console.log(allIssues);
     });
 };
 const displayShow = (id) => {
   const cardContainer = document.getElementById("card-container");
   for (data of id) {
     let borderColor = "";
-    let statusColor = ""
+    let statusColor = "";
 
-// status Color dynamic
+    // status Color dynamic
     if (data.priority === "high") {
       statusColor = "bg-[#ef444430] text-[#ef4444]";
-    } 
-    else if (data.priority === "medium") {
+    } else if (data.priority === "medium") {
       statusColor = "bg-[#FFF8DB] text-[#D97706]";
-    } 
-    else {
+    } else {
       statusColor = "bg-gray-200 text-gray-500";
     }
-// border color dynamic
+    // border color dynamic
     if (data.priority === "low") {
       borderColor = "border-[#A855F7]";
-    } 
-    else {
+    } else {
       borderColor = "border-green-500";
     }
     const div = document.createElement("div");
@@ -40,10 +49,8 @@ const displayShow = (id) => {
         <h2 class="font-bold text-xl">${data.title}</h2>
         <p class="text-gray-500">${data.description}</p>
         <div class="flex space-x-3">
-          <p class="py-1 px-3 font-semibold bg-[#ef444430] text-[#ef4444] rounded-2xl border border-[#ef4444]"><i
-              class="fa-solid fa-bugs"></i>${data.labels[0].toUpperCase()}</p>
-          <p class="py-1 px-7 font-semibold bg-[#FFF8DB] text-[#D97706] rounded-2xl border border-[#FDE68A]"><i
-              class="fa-regular fa-life-ring"></i>${data.status}</p>
+          <p>${problemStatus(data.labels)}</p>
+        
         </div>
         <hr class="text-gray-300 my-4">
         <div class="">
@@ -56,7 +63,7 @@ const displayShow = (id) => {
     cardContainer.append(div);
   }
 
-const counter = document.getElementById('counter')
+  const counter = document.getElementById("counter");
   const countProblem = id.length;
   counter.innerText = countProblem;
 };
